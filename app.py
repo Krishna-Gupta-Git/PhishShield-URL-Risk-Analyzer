ac0035c8f456d02bd3b0758f9b9b74b23b8729b4
Krishna <apk.krishnagupta@gmail.com>
2026-07-11 23:49:07 +0530

    Add Recommendation and Improving Result

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ app = Flask(__name__)
 def home():
     return render_template("index.html")
     url = request.form["url"]
-    
+
 @app.route("/analyze", methods=["POST"])
 def analyze():
     url = request.form["url"]
@@ -24,14 +24,23 @@ def analyze():
     else:
         risk_level = "High Risk"
 
+    if risk_level == "Low Risk":
+        recommendation = "This website appears safe, but always verify before entering personal information."
+
+    elif risk_level == "Medium Risk":
+        recommendation = "Proceed carefully. Verify the website before logging in or sharing sensitive information."
+
+    else:
+        recommendation = "Avoid visiting this website. It contains several phishing indicators."
     return render_template(
-        "result.html",
-        url=url,
-        result=result,
-        score=score,
-        reasons=reasons,
-        risk_level=risk_level
-    )
+    "result.html",
+    url=url,
+    result=result,
+    score=score,
+    reasons=reasons,
+    risk_level=risk_level,
+    recommendation=recommendation
+)
 
 if __name__ == "__main__":
     app.run(debug=True)
